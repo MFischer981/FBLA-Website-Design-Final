ae0d67d55529c355056d7a1fe5d96998e5533b3e
Micah Fischer <42282649+MFischer981@users.noreply.github.com>
2019-03-20 14:04:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function showAnswer() {
+   event.target.parentNode.classList.toggle('center'); 
+   event.target.parentNode.childNodes[5].classList.toggle('show'); 
+   event.target.classList.toggle('click'); 
+}
+
